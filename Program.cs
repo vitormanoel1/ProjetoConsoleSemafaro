f5b6c176d6eea1fbdfd7f77cd03e28ecc10d0feb
vitor20022003 <vitorm4123@gmail.com>
2020-11-13 03:55:25 -0300

    algoritmo semafaro concluido

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace semafaro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a cor do semaforo (vermelho, amarelo, verde):");
+            string cordosem = (Console.ReadLine());
+
+            if(cordosem == "vermelho") {
+                Console.Write("Pare!");
+                    }
+            else if(cordosem == "amarelo") {
+                Console.Write("Reduzir a velocidade e Para");
+                    }
+            else {
+                Console.Write("continuar acelerando");
+                    }   
+            
+        }
+    }
+}
